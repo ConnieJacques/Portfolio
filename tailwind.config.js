f57132ef9a5efa36dacdb42b04e46ebe0a929e24
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-06-06 16:18:32 +1000

    styling: google font for my name in the header added to index.html and tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         'light-green': '#DFF5EF',
         'dark-green': '#308C88'
+      },
+      fontFamily: {
+        'cursive-header': 'Homemade Apple, cursive',
       }
     },
   },
