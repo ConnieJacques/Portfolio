4b9f09a1985ee9ee6d4cc0316a86b0f785f55a5f
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-06-07 19:56:00 +1000

    styling: home page styling altered for responsiveness

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,7 @@
 export function HomePage() {
     return(
         <div className="sm:h-5/6">
-            <p className="font-courier text-5xl px-12 py-28 leading-tight sm:text-6xl sm:leading-relaxed md:text-8xl md:leading-snug lg:text-8xl lg:py-16 lg:px-24 lg:text-justify"> Hi, I'm Connie and I'm a Back-End Web Developer.</p>
+            <p className="z-2 absolute top-0 left-0 font-courier text-5xl px-12 py-28 leading-tight sm:text-6xl sm:leading-relaxed md:text-8xl md:leading-snug lg:text-8xl lg:py-16 lg:px-24 lg:text-justify"> Hi, I'm Connie and I'm a Back-End Web Developer.</p>
         </div>
     )
 }
