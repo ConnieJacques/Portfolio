90533ae37bb21bdd77f09823b5598e14d18541e5
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-06-07 16:53:13 +1000

    feat: HomePage begun

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,7 @@
 export function HomePage() {
     return(
         <div>
+            <p className="font-courier text-4xl"> Hi, I'm Connie and I'm a Back-End Web Developer.</p>
         </div>
     )
 }
