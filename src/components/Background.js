5153b7bc95c37e9c461f4a34ba407bc65e86984b
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-06-07 19:52:32 +1000

    feat: background styling added

---
+++ src/components/Background.js
@@ -0,0 +1,13 @@
+
+
+
+export function Background() {
+    return (
+        <div>
+            <div className="border-r-4 h-[80vh] w-[90vw] -z-2 absolute border-light-green"></div>
+            <div className="border-r-4 h-[80vh] w-[92vw] -z-3 absolute border-light-green"></div>
+            <div className="firstXLine"></div>
+            <div className="secondXLine"></div>
+        </div>
+    )
+} 
