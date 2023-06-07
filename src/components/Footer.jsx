052146147c324afbdd40de9c8ad3bb25c7944298
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-06-07 20:48:53 +1000

    feat: external links added to all icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,12 +6,23 @@ import { Copyright } from './Copyright'
 
 
 export function Footer() {
+    const email = "connie.jacques91@gmail.com";
+    const subject = "Interest in Web Development Work";
+
     return (
         <div className='flex flex-row justify-between border-t-2 border-dark-green p-4'>
-            <img src={emailIcon} alt="GitHub Icon" className="h-8 w-8"/>
-            <img src={linkedInIcon} alt="GitHub Icon" className="h-8 w-8"/>
-            <img src={gitHubIcon} alt="GitHub Icon" className="h-8 w-8"/>
-            <img src={etsyIcon} alt="GitHub Icon" className="h-8 w-8"/>
+            <a href={`mailto:${email}?subject=${encodeURIComponent(subject)}`}>
+                <img src={emailIcon} alt="GitHub Icon" className="h-8 w-8"/>
+            </a>
+            <a href="https://www.linkedin.com/in/connie-jacques-53046b261/" target="_blank" rel="noreferrer">
+                <img src={linkedInIcon} alt="GitHub Icon" className="h-8 w-8"/>
+            </a>
+            <a href="https://github.com/ConnieJacques" target="_blank" rel="noreferrer">
+                <img src={gitHubIcon} alt="GitHub Icon" className="h-8 w-8"/>
+            </a>
+            <a href="https://www.etsy.com/au/shop/CoenobitaCollective" target="_blank" rel="noreferrer">
+                <img src={etsyIcon} alt="GitHub Icon" className="h-8 w-8"/>
+            </a>
             <Copyright />
         </div>
 
