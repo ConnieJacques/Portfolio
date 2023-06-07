4f43f4d072fad707461c7c6a385d5133c0c04213
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-06-07 16:52:09 +1000

    feat!: routes added to App.js, PageLayout formated to organise pages for SPA

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 // import logo from './logo.svg';
 import './App.css';
-import { Header } from './components/Header';
+import { Route, Routes } from "react-router-dom";
+import { PageLayout } from './Layout/PageLayout';
+import { HomePage } from './pages/HomePage'
+import { AboutMePage} from './pages/AboutMePage';
+import { ContactPage } from './pages/ContactPage';
+import { ProjectsPage} from './pages/ProjectsPage';
+
 
 
 function App() {
   return (
     <div className="webkitDefault">
-      <Header />
+      <Routes>
+          <Route element={<PageLayout />}>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/projects" element={<ProjectsPage />} />
+              <Route path="/about" element={<AboutMePage />} />
+              <Route path="/contact" element={<ContactPage />} />
+          </Route>
+      </Routes>
     </div>
   );
 }
--- src/Layout/PageLayout.jsx
+++ src/Layout/PageLayout.jsx
@@ -2,12 +2,14 @@ import { Header } from '../components/Header';
 // import { Background } from './components/Background';
 import { Footer } from '../components/Footer';
 import { Outlet } from "react-router-dom";
+// import { Background } from '../components/Background'
 
 
 export function PageLayout(){
     return(
         <div>
             <Header />
+            {/* <Background /> */}
                 <Outlet/>
             <Footer />
         </div> 
