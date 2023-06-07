3b9a11e11a7e94269f261594671e624a4be3b22c
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-06-07 17:22:51 +1000

    styling: home partially styled for responsive design

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,8 +2,8 @@
 
 export function HomePage() {
     return(
-        <div>
-            <p className="font-courier text-4xl"> Hi, I'm Connie and I'm a Back-End Web Developer.</p>
+        <div className="sm:h-5/6">
+            <p className="font-courier text-5xl px-12 py-28 leading-tight sm:text-6xl sm:leading-relaxed md:text-8xl md:leading-snug lg:text-8xl lg:py-16 lg:px-24 lg:text-justify"> Hi, I'm Connie and I'm a Back-End Web Developer.</p>
         </div>
     )
 }
