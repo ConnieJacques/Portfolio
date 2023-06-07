5660c7a966221dad5696cef2234c59ef2d0012bb
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-06-07 20:21:52 +1000

    feat: copyright information and date logic moved into seperate component and styled for mobile view

---
+++ src/components/Copyright.jsx
@@ -0,0 +1,10 @@
+
+
+
+export function Copyright() {
+    const year = new Date().getFullYear()
+
+    return (
+        <p className="font-railway text-dark-green text-end text-sm flex flex-column items-center">© Connie Jacques {year} </p>
+    )
+}
