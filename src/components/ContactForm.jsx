cb76cd79274d78ee19bb530d37d9805052c6f8fc
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-06-17 16:50:15 +1000

    feat: form working as intended and styled for mobile and laptop

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -17,14 +17,26 @@ export function ContactForm() {
     }
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label className='text-white'>Name:</label>
-      <input type="text" name="user_name" defaultValue={null} className='text-black' />
-      <label className='text-white'>Email:</label>
-      <input type="email" name="user_email" className='text-black' />
-      <label className='text-white'>Message:</label>
-      <textarea name="message" className='text-red' />
-      <input type="submit" value="Send" className='bg-white' />
-    </form>
+    <section className='h-[80vh] sm:h-[85vh]'>
+        <div className='-z-1 absolute top-20 w-[85vw] font-raleway left-0 text-white text-base px-12 pt-24 xl:pt-8 sm:text-6xl md:text-8xl lg:text-lg lg:py-16 lg:px-24'>
+            
+            <div className='inline-flex w-min'>
+                <p className='font-raleway font-bold tracking-widest uppercase font-bold text-4xl text-saturated-green underline underline-offset-4 decoration-dark-green'>Send a messag</p>
+                <p className='font-raleway font-bold self-end uppercase font-bold text-4xl text-saturated-green underline underline-offset-4 decoration-dark-green'>e</p>
+            </div>
+
+            <form ref={form} onSubmit={sendEmail} className='flex flex-col w-[70vw] xl:w-[35vw]'>
+                <label className='text-white pt-12 pb-1'>Name:</label>
+                    <input type="text" name="user_name" defaultValue={null} className='text-black bg-saturated-green pl-2 rounded-md focus:outline focus:outline-dark-green focus:outline-2 focus:outline-offset-2' />
+                <label className='text-white pt-6 pb-1'>Email:</label>
+                    <input type="email" name="user_email" className='text-black bg-saturated-green pl-2 rounded-md focus:outline focus:outline-dark-green focus:outline-2 focus:outline-offset-2' />
+                <label className='text-white pt-6 pb-1'>Message:</label>
+                    <textarea name="message" className='text-black bg-saturated-green pl-2 rounded-md resize-none focus:outline focus:outline-dark-green focus:outline-2 focus:outline-offset-2' />
+                <div className='pt-6'>
+                    <input type="submit" value="Send" className='bg-saturated-green text-dark-green font-bold uppercase py-1 px-4 rounded-md hover:outline hover:outline-dark-green hover:outline-2 hover:outline-offset-2' />
+                </div>
+            </form>
+        </div>
+    </section>
   )
 }
