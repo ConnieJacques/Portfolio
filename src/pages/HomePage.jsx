fa222a81f8f640ec8379613d65170e8de0d5654d
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-06-17 18:33:19 +1000

    fix!: broken css preventing page for displaying rendered content fixed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@
 
 export function HomePage() {
     return(
-        <div className="h-[80] sm:h-[85vh]">
+        <div className="h-[80vh] sm:h-[85vh]">
             <p className="-z-1 absolute top-20 left-0 font-courier text-white text-4xl px-12 pt-44 leading-tight sm:text-6xl sm:leading-relaxed md:text-8xl md:leading-snug lg:text-xl lg:py-16 lg:px-24 lg:text-justify"> Hi, I'm Connie and I'm a Back-End Web Developer.</p>
         </div>
     )
