e596e2aa7896f6eb96a6213a4bcd485dbd7dae3e
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-06-18 18:03:07 +1000

    feat: required form attribute added to all form field for validation

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -30,11 +30,11 @@ export function ContactForm() {
 
             <form ref={form} onSubmit={sendEmail} className='flex flex-col w-[70vw] xl:w-[35vw]'>
                 <label className='text-white pt-12 pb-1'>Name:</label>
-                    <input type="text" name="user_name" defaultValue={null} className='text-black bg-saturated-green pl-2 rounded-md focus:outline focus:outline-dark-green focus:outline-2 focus:outline-offset-2' />
+                    <input type="text" required name="user_name" defaultValue={null} className='text-black bg-saturated-green pl-2 rounded-md focus:outline focus:outline-dark-green focus:outline-2 focus:outline-offset-2' />
                 <label className='text-white pt-6 xl:pt-4 pb-1'>Email:</label>
-                    <input type="email" name="user_email" className='text-black bg-saturated-green pl-2 rounded-md focus:outline focus:outline-dark-green focus:outline-2 focus:outline-offset-2' />
+                    <input type="email" required name="user_email" className='text-black bg-saturated-green pl-2 rounded-md focus:outline focus:outline-dark-green focus:outline-2 focus:outline-offset-2' />
                 <label className='text-white pt-6 xl:pt-4 pb-1'>Message:</label>
-                    <textarea name="message" className='text-black bg-saturated-green pl-2 rounded-md resize-none focus:outline focus:outline-dark-green focus:outline-2 focus:outline-offset-2' />
+                    <textarea name="message" required className='text-black bg-saturated-green pl-2 rounded-md resize-none focus:outline focus:outline-dark-green focus:outline-2 focus:outline-offset-2' />
                 <div className='pt-6'>
                     <input type="submit" value="Send" className='bg-saturated-green text-dark-green font-bold uppercase py-1 px-4 rounded-md hover:outline hover:outline-dark-green hover:outline-2 hover:outline-offset-2' />
                 </div>
