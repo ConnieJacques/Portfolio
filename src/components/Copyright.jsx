93e8162dec96773e636a2e7a906f0276e9710cdb
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-06-24 13:56:30 +1000

    feat: conditional rendering added to footer to display copyright notice instead of contact icons on Contact page. Styled for mobile and laptop

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -5,6 +5,6 @@ export function Copyright() {
     const year = new Date().getFullYear()
 
     return (
-        <p className="font-railway text-dark-green text-end text-sm flex flex-column items-center">© Connie Jacques {year} </p>
+        <p className="h-[10dvh] flex flex-row justify-center border-t border-dark-green font-railway text-dark-green text-end text-sm flex flex-column items-center">© Connie Jacques {year}. All rights reserved </p>
     )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,14 @@
-import gitHubIcon from '../images/icons8-github.svg'
-import linkedInIcon from '../images/icons8-linkedin.svg'
-import emailIcon from '../images/icons8-email-50.png'
-import etsyIcon from '../images/icons8-etsy.svg'
-// import { Copyright } from './Copyright'
+import { useLocation } from "react-router-dom";
+import { Copyright } from './Copyright'
+import { FooterContact } from "./FooterContact";
 
 
 export function Footer() {
-    const email = "connie.jacques91@gmail.com";
-    const subject = "Interest in Web Development Work";
+    const location = useLocation()
 
     return (
-        <div className='flex flex-row justify-between items-center md:justify-center border-t border-dark-green py-4 px-6 h-[10dvh]'>
-            <a href={`mailto:${email}?subject=${encodeURIComponent(subject)}`}>
-                <img src={emailIcon} alt="GitHub Icon" className="h-8 w-8 md:mr-3"/>
-            </a>
-            <a href="https://www.linkedin.com/in/connie-jacques-53046b261/" target="_blank" rel="noreferrer">
-                <img src={linkedInIcon} alt="GitHub Icon" className="h-8 w-8 md:mr-3"/>
-            </a>
-            <a href="https://github.com/ConnieJacques" target="_blank" rel="noreferrer">
-                <img src={gitHubIcon} alt="GitHub Icon" className="h-8 w-8 md:mr-3"/>
-            </a>
-            <a href="https://www.etsy.com/au/shop/CoenobitaCollective" target="_blank" rel="noreferrer">
-                <img src={etsyIcon} alt="GitHub Icon" className="h-9 w-9"/>
-            </a>
-            {/* <Copyright /> */}
+        <div className='h-[10dvh]'>
+            {(location.pathname === '/contact') ? <Copyright /> : <FooterContact />}
         </div>
     )
 }
