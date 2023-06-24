15353b32ba250cadd1264b4e494a50de85c0b74f
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-06-24 15:50:40 +1000

    feat: education component added for About Me page. Styled for mobile and laptop

--- src/pages/AboutMePage.jsx
+++ src/pages/AboutMePage.jsx
@@ -1,4 +1,5 @@
 
+import { EducationComponent } from "../components/AboutEducation";
 import { AboutMeComponent } from "../components/AboutMeComponent";
 import { VeterinaryOverlap } from "../components/AboutOverlapVeterinary";
 import { TechComponent } from "../components/AboutTechStack";
@@ -20,7 +21,8 @@ export function AboutMePage() {
                                     <a href="#item1" className='h-3 w-3 border-dark-green rounded-full border-2 text-transparent mb-2 ml-[0.32rem]'>.</a>
                                     <a href="#item2" className='h-3 w-3 bg-dark-green rounded-full text-transparent mb-2 ml-[0.32rem]'>.</a>
                                     <a href="#item3" className='h-3 w-3 bg-dark-green rounded-full text-transparent mb-2 ml-[0.32rem]'>.</a> 
-                                    <a href="#item4" className='h-3 w-3 bg-dark-green rounded-full text-transparent ml-[0.32rem]'>.</a> 
+                                    <a href="#item4" className='h-3 w-3 bg-dark-green rounded-full text-transparent mb-2 ml-[0.32rem]'>.</a> 
+                                    <a href="#item5" className='h-3 w-3 bg-dark-green rounded-full text-transparent ml-[0.32rem]'>.</a>
                                     <a href="#item2" className="h-[1.7rem] text-dark-green font-ariel rotate-180 text-4xl">^</a>
                                 </div>
                             </div>
@@ -35,7 +37,8 @@ export function AboutMePage() {
                                         <a href="#item1" className='h-3 w-3 bg-dark-green rounded-full text-transparent mb-2 ml-[0.32rem]'>.</a>
                                         <a href="#item2" className='h-3 w-3 border-dark-green rounded-full border-2 text-transparent mb-2 ml-[0.32rem]'>.</a>
                                         <a href="#item3" className='h-3 w-3 bg-dark-green rounded-full text-transparent mb-2 ml-[0.32rem]'>.</a> 
-                                        <a href="#item4" className='h-3 w-3 bg-dark-green rounded-full text-transparent ml-[0.32rem]'>.</a> 
+                                        <a href="#item4" className='h-3 w-3 bg-dark-green rounded-full text-transparent mb-2 ml-[0.32rem]'>.</a>
+                                        <a href="#item5" className='h-3 w-3 bg-dark-green rounded-full text-transparent ml-[0.32rem]'>.</a> 
                                         <a href="#item3" className="h-[1.7rem] text-dark-green font-ariel rotate-180 text-4xl">^</a>
                                     </div>
                                 </div>
@@ -50,7 +53,8 @@ export function AboutMePage() {
                                         <a href="#item1" className='h-3 w-3 bg-dark-green rounded-full text-transparent mb-2 ml-[0.32rem]'>.</a>
                                         <a href="#item2" className='h-3 w-3 bg-dark-green rounded-full text-transparent mb-2 ml-[0.32rem]'>.</a>
                                         <a href="#item3" className='h-3 w-3 border-dark-green rounded-full border-2 text-transparent mb-2 ml-[0.32rem]'>.</a> 
-                                        <a href="#item4" className='h-3 w-3 bg-dark-green rounded-full text-transparent mb-2 ml-[0.32rem]'>.</a> 
+                                        <a href="#item4" className='h-3 w-3 bg-dark-green rounded-full text-transparent mb-2 ml-[0.32rem]'>.</a>
+                                        <a href="#item5" className='h-3 w-3 bg-dark-green rounded-full text-transparent ml-[0.32rem]'>.</a> 
                                         <a href="#item4" className="h-[1.7rem] text-dark-green font-ariel rotate-180 text-4xl">^</a>
                                     </div>
                                 </div>
@@ -58,15 +62,32 @@ export function AboutMePage() {
                 </div> 
                 <div id='item4' className="carousel-item h-full">
                     <TechComponent />
-                        <div className='relative top-[25dvh] left-[-90dvw]'>
+                        <div className='relative top-[25dvh] left-[-90dvw] xl:left-[-90dvw]'>
                                 <div className='bg-saturated-green p-2 rounded-r-2xl'>
                                     <div className="flex flex-col">
                                         <a href="#item3" className="h-[1.7rem] text-dark-green font-ariel text-4xl">^</a> 
                                         <a href="#item1" className='h-3 w-3 bg-dark-green rounded-full text-transparent mb-2 ml-[0.32rem]'>.</a>
                                         <a href="#item2" className='h-3 w-3 bg-dark-green rounded-full text-transparent mb-2 ml-[0.32rem]'>.</a>
                                         <a href="#item3" className='h-3 w-3 bg-dark-green rounded-full text-transparent mb-2 ml-[0.32rem]'>.</a> 
-                                        <a href="#item4" className='h-3 w-3 border-dark-green rounded-full border-2 text-transparent ml-[0.32rem]'>.</a> 
-                                        <a href="#item4" className="h-[1.7rem] text-dark-green font-ariel rotate-180 text-4xl">^</a>
+                                        <a href="#item4" className='h-3 w-3 border-dark-green rounded-full border-2 mb-2 text-transparent ml-[0.32rem]'>.</a>
+                                        <a href="#item5" className='h-3 w-3 bg-dark-green rounded-full text-transparent ml-[0.32rem]'>.</a> 
+                                        <a href="#item5" className="h-[1.7rem] text-dark-green font-ariel rotate-180 text-4xl">^</a>
+                                    </div>
+                                </div>
+                            </div>
+                </div>
+                <div id='item5' className="carousel-item h-full">
+                    <EducationComponent />
+                        <div className='relative top-[25dvh] left-[-90dvw] xl:left-[-90dvw'>
+                                <div className='bg-saturated-green p-2 rounded-r-2xl'>
+                                    <div className="flex flex-col">
+                                        <a href="#item4" className="h-[1.7rem] text-dark-green font-ariel text-4xl">^</a> 
+                                        <a href="#item1" className='h-3 w-3 bg-dark-green rounded-full text-transparent mb-2 ml-[0.32rem]'>.</a>
+                                        <a href="#item2" className='h-3 w-3 bg-dark-green rounded-full text-transparent mb-2 ml-[0.32rem]'>.</a>
+                                        <a href="#item3" className='h-3 w-3 bg-dark-green rounded-full text-transparent mb-2 ml-[0.32rem]'>.</a>
+                                        <a href="#item4" className='h-3 w-3 bg-dark-green rounded-full text-transparent mb-2 ml-[0.32rem]'>.</a> 
+                                        <a href="#item5" className='h-3 w-3 border-dark-green rounded-full border-2 text-transparent ml-[0.32rem]'>.</a> 
+                                        <a href="#item5" className="h-[1.7rem] text-dark-green font-ariel rotate-180 text-4xl">^</a>
                                     </div>
                                 </div>
                             </div>
