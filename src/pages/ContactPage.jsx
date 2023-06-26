4f4279c15e8466080334915eb08a05223af226a1
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-06-26 10:54:43 +1000

    feat: carousel navigation arrows altered to make a continuous cycle

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -13,7 +13,7 @@ export function ContactPage() {
                                 <div className='relative top-[15dvh] left-[-85dvw]'>
                                         <div className='bg-saturated-green p-2 rounded-r-2xl'>
                                             <div className="flex flex-col">
-                                                <a href="#item1" className="h-[1.7rem] text-dark-green font-ariel text-4xl">^</a> 
+                                                <a href="#item2" className="h-[1.7rem] text-dark-green font-ariel text-4xl">^</a> 
                                                 <a href="#item1" className='h-3 w-3 border-dark-green rounded-full border-2 text-transparent mb-2 ml-[0.32rem]'>.</a>
                                                 <a href="#item2" className='h-3 w-3 bg-dark-green rounded-full text-transparent ml-[0.32rem]'>.</a>
                                                 <a href="#item2" className="h-[1.7rem] text-dark-green font-ariel rotate-180 text-4xl">^</a>
@@ -29,7 +29,7 @@ export function ContactPage() {
                                             <a href="#item1" className="h-[1.7rem] text-dark-green font-ariel text-4xl">^</a> 
                                             <a href="#item1" className='h-3 w-3 bg-dark-green rounded-full text-transparent mb-2 ml-[0.32rem]'>.</a>
                                             <a href="#item2" className='h-3 w-3 border-dark-green rounded-full border-2 text-transparent ml-[0.32rem]'>.</a>
-                                            <a href="#item2" className="h-[1.7rem] text-dark-green font-ariel rotate-180 text-4xl">^</a>
+                                            <a href="#item1" className="h-[1.7rem] text-dark-green font-ariel rotate-180 text-4xl">^</a>
                                         </div>
                                     </div>
                                 </div>
--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -12,7 +12,7 @@ export function ProjectsPage() {
                         <div className='relative top-[25dvh] left-[-90vw] xl:left-[-90dvw]'>
                             <div className='bg-saturated-green p-2 rounded-r-2xl'>
                                 <div className="flex flex-col">
-                                    <a href="#item1" className="h-[1.7rem] text-dark-green font-ariel text-4xl">^</a> 
+                                    <a href="#item3" className="h-[1.7rem] text-dark-green font-ariel text-4xl">^</a> 
                                     <a href="#item1" className='h-3 w-3 border-dark-green rounded-full border-2 text-transparent mb-2 ml-[0.32rem]'>.</a>
                                     <a href="#item2" className='h-3 w-3 bg-dark-green rounded-full text-transparent mb-2 ml-[0.32rem]'>.</a>
                                     <a href="#item3" className='h-3 w-3 bg-dark-green rounded-full text-transparent ml-[0.32rem]'>.</a>  
@@ -44,7 +44,7 @@ export function ProjectsPage() {
                                         <a href="#item1" className='h-3 w-3 bg-dark-green rounded-full text-transparent mb-2 ml-[0.32rem]'>.</a>
                                         <a href="#item2" className='h-3 w-3 bg-dark-green rounded-full text-transparent mb-2 ml-[0.32rem]'>.</a>
                                         <a href="#item3" className='h-3 w-3 border-dark-green rounded-full border-2 text-transparent ml-[0.32rem]'>.</a> 
-                                        <a href="#item3" className="h-[1.7rem] text-dark-green font-ariel rotate-180 text-4xl">^</a>
+                                        <a href="#item1" className="h-[1.7rem] text-dark-green font-ariel rotate-180 text-4xl">^</a>
                                     </div>
                                 </div>
                             </div>
