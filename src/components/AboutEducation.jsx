a8e98d673fbd37b94ef9f14c8631dccc5d31b3dc
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-06-26 12:47:38 +1000

    styling: text formating altered on enducation component for contenuity with the rest of the website

--- src/components/AboutEducation.jsx
+++ src/components/AboutEducation.jsx
@@ -14,22 +14,22 @@ export function EducationComponent() {
             </div >
             <p className='text-white text-sm xl:text-xl w-[70dvw] xl:w-[85dvw] xl:h-[50dvh] xl:text-sm xl:flex xl:flex-row xl:flex-wrap'>
                 <div className='xl:w-[30dvw]'>
-                    <h4 className='uppercase tracking-widest font-bold'>Diploma of Information Technology</h4>
+                    <h4 className='uppercase tracking-widest xl:tracking-[0.2rem] font-bold'>Diploma of Information Technology</h4>
                     <h5 className='xl:py-2'>Coder Academy</h5>
                     <h6>2022 - 2023</h6>
                 </div>
                 <div className='xl:w-[40dvw] xl:pl-36'>
-                    <h4 className='uppercase tracking-widest font-bold'>Certificate IV in Veterinary Nursing</h4>
+                    <h4 className='uppercase tracking-widest xl:tracking-[0.2rem] font-bold'>Certificate IV in Veterinary Nursing</h4>
                     <h5 className='xl:py-2'>TAFE NSW</h5>
                     <h6>2014 - 2016</h6>
                 </div>
                 <div className='xl:w-[30dvw]'>
-                    <h4 className='uppercase tracking-widest font-bold pt-4'>Bachelor of Media and Communications (Honours)</h4>
+                    <h4 className='uppercase tracking-widest xl:tracking-[0.2rem] font-bold pt-4'>Bachelor of Media and Communications (Honours)</h4>
                     <h5 className='xl:py-2'>University of New Englandy</h5>
                     <h6>2013</h6>
                 </div>
                 <div className='xl:w-[40dvw] xl:pl-36'>
-                    <h4 className='uppercase tracking-widest font-bold pt-4'>Bachelor of Arts (writing and philosophy)</h4>
+                    <h4 className='uppercase tracking-widest xl:tracking-[0.2rem] font-bold pt-4'>Bachelor of Arts (writing and philosophy)</h4>
                     <h5 className='xl:py-2'>University of New England</h5>
                     <h6>2010 - 2012</h6>
                 </div>
