5b6edce0d3bc09bb8cb470c4fff272aaf4e6a36b
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-06-26 12:44:27 +1000

    feat: projects page layout finalised, styling complete for mobile and laptop views

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,4 +1,6 @@
+import KingAPIComponent from "../components/KingAPIComponent";
 import MoonswellComponent from "../components/MoonswellComponent";
+import TerminalAppGameComponent from "../components/TerminalAppGameComponent";
 
 
 
@@ -8,7 +10,7 @@ export function ProjectsPage() {
         <div className='absolute z-1 top-[10dvh]'>
             <div className="carousel carousel-vertical w-[90vw] h-[80dvh] pt-0">
                 <div id='item1' className="carousel-item h-full">
-                    <MoonswellComponent />
+                    <TerminalAppGameComponent />
                         <div className='relative top-[25dvh] left-[-90vw] xl:left-[-90dvw]'>
                             <div className='bg-saturated-green p-2 rounded-r-2xl'>
                                 <div className="flex flex-col">
@@ -22,8 +24,8 @@ export function ProjectsPage() {
                         </div>
                 </div> 
                 <div id='item2' className="carousel-item h-full">
-                    {/* <VeterinaryComponent /> */}
-                        <div className='relative top-[25dvh] left-[-90dvw] xl:left-[-87.5dvw]'>
+                    <KingAPIComponent />
+                        <div className='relative top-[25dvh] left-[-90dvw] xl:left-[-90dvw]'>
                                 <div className='bg-saturated-green p-2 rounded-r-2xl'>
                                     <div className="flex flex-col">
                                         <a href="#item1" className="h-[1.7rem] text-dark-green font-ariel text-4xl">^</a> 
@@ -36,8 +38,8 @@ export function ProjectsPage() {
                             </div>
                 </div> 
                 <div id='item3' className="carousel-item h-full">
-                    {/* <VeterinaryOverlap /> */}
-                        <div className='relative top-[25dvh] left-[-90dvw] xl:left-[-87.5dvw]'>
+                    <MoonswellComponent />
+                        <div className='relative top-[25dvh] left-[-90dvw] xl:left-[-90dvw]'>
                                 <div className='bg-saturated-green p-2 rounded-r-2xl'>
                                     <div className="flex flex-col">
                                         <a href="#item2" className="h-[1.7rem] text-dark-green font-ariel text-4xl">^</a> 
